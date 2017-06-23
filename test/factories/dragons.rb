89af9c0307b5c411ddc2f75cf40906935a2e5705
Roopesh <rrattan.89@gmail.com>
2017-06-23 14:51:43 -0400

    testing methods + add age attributes & retest

--- test/factories/dragons.rb
+++ test/factories/dragons.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :dragon do
     name "Trogdor"
+
   end
 end
--- test/models/dragon_test.rb
+++ test/models/dragon_test.rb
@@ -19,28 +19,27 @@ class DragonTest < ActiveSupport::TestCase
 
   def test_that_young_dragons_from_asoiaf
     # use your knowledge of factories to make this test pass!
-    young_dragon = nil
+    young_dragon = build(:dragon, age: 2)
 
     # not allowed to change the assert statement!
-    skip unless young_dragon
     assert_equal( 'A Song of Ice and Fire', young_dragon.period_alive)
   end
 
   def test_that_middle_dragons_from_targ_dynasty
     # use your knowledge of factories to make this test pass!
-    middle_dragon = nil
+    middle_dragon = build(:dragon, age: 111)
 
     # not allowed to change the assert statement!
-    skip unless middle_dragon
+
     assert_equal( 'Targaryen Dynasty', middle_dragon.period_alive)
   end
 
   def test_that_ancient_dragons_from_age_of_valyria
     # use your knowledge of factories to make this test pass!
-    ancient_dragon = nil
+    ancient_dragon = build(:dragon, age: 500)
 
     # not allowed to change the assert statement!
-    skip unless ancient_dragon
+
     assert_equal( 'Age of Valyria', ancient_dragon.period_alive)
   end
 
